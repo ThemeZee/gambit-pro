a4b4f84c9a88192e59e33a137a6b6281f2a4a8e8
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-01-31 14:39:36 +0100

    changed markup of header bar

--- gambit-pro.php
+++ gambit-pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://themezee.com/addons/gambit-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the Gambit theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0.2
+Version: 1.0.3
 Text Domain: gambit-pro
 Domain Path: /languages/
 License: GPL v3
@@ -62,7 +62,7 @@ class Gambit_Pro {
 		define( 'GAMBIT_PRO_NAME', 'Gambit Pro' );
 
 		// Define Version Number.
-		define( 'GAMBIT_PRO_VERSION', '1.0.2' );
+		define( 'GAMBIT_PRO_VERSION', '1.0.3' );
 
 		// Define Plugin Name.
 		define( 'GAMBIT_PRO_PRODUCT_ID', 73300 );
