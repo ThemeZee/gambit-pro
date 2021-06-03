74957184b0e7c090caa78448f6851e67b31b3f64
Thomas Weichselbaumer <kontakt@themecoder.de>
2021-06-03 14:48:12 +0200

    added svg icon to scroll to top button

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -49,9 +49,23 @@ class Gambit_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'gambit-pro-scroll-to-top', GAMBIT_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210212', true );
+			wp_enqueue_script( 'gambit-pro-scroll-to-top', GAMBIT_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210603', true );
+
+			// Passing Parameters to navigation.js.
+			wp_localize_script( 'gambit-pro-scroll-to-top', 'gambitProScrollToTop', array( 'icon' => self::get_svg( 'collapse' ) ) );
 
 		endif;
+	}
+
+	/**
+	 * Get SVG icon.
+	 *
+	 * @return void
+	 */
+	static function get_svg( $icon ) {
+		if ( function_exists( 'gambit_get_svg' ) ) {
+			return gambit_get_svg( $icon );
+		}
 	}
 
 	/**
