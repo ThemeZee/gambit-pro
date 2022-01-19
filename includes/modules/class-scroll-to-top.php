001a45f09fe5ee6e0cfc1fe6ac9e131196093ff1
Thomas Weichselbaumer <kontakt@themecoder.de>
2022-01-19 17:19:51 +0100

    updated Scroll to Top button script to use vanilla js

--- includes/modules/class-scroll-to-top.php
+++ includes/modules/class-scroll-to-top.php
@@ -49,7 +49,7 @@ class Gambit_Pro_Scroll_To_Top {
 		// Call Credit Link function of theme if credit link is activated.
 		if ( true === $theme_options['scroll_to_top'] && ! self::is_amp() ) :
 
-			wp_enqueue_script( 'gambit-pro-scroll-to-top', GAMBIT_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array( 'jquery' ), '20210603', true );
+			wp_enqueue_script( 'gambit-pro-scroll-to-top', GAMBIT_PRO_PLUGIN_URL . 'assets/js/scroll-to-top.min.js', array(), '20220119', true );
 
 			// Passing Parameters to navigation.js.
 			wp_localize_script( 'gambit-pro-scroll-to-top', 'gambitProScrollToTop', array( 'icon' => self::get_svg( 'collapse' ) ) );
